e67f74ecace47733b69463adf6e8cb55bf7e1887
Agnes Park <agnespark824@gmail.com>
2020-01-28 17:42:49 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,14 @@
-# Your code goes here!
+class Dog
+  def name=(puppers_name) 
+    @this_puppers_name = puppers_name
+  end
+
+  def name 
+    @this_puppers_name
+  end
+  
+  def bark
+    puts "woof!"
+  end
+end
+

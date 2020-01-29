b79322f556fc1b009ebc41e5a94fd1b968796900
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-29 18:19:19 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,19 @@
 # Your code goes here!
+class Dog 
+  Rocky = dog.new
+  def name = 'Rocky'
+    @name = Rocky
+  end
+  def bark
+    puts "woof"
+  end
+  
+  def name.bark
+    name.bark = 'Rocky.bark'
+    puts "name.bark" woof!.
+  end
+end
+    
+    
+    
+  end

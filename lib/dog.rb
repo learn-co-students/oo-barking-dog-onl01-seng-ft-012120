156696f808228710e208a9f24046a6a7ab02b25e
thisistheway369 <thisistheway369@icloud.com>
2020-01-30 21:52:58 -0800

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,26 @@
 # Your code goes here!
+# OBJECTIVES
+
+# Practice defining a class
+# Build instance methods––both setter and getter
+
+# Your code goes here!
+
+class Dog
+    def name=(dogs_name)
+      @name = dogs_name
+    end 
+  
+    def name
+      @name
+    end 
+    
+  end 
+  
+  class Dog
+    def bark
+      puts "woof!"
+    end 
+    
+  end 
+  
